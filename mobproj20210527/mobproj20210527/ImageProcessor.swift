d9de73928bc1884fbd769339add70306afa054f6
Ike Mattice <ikemattice@icloud.com>
2021-05-27 19:02:27 -0700

    write file to disk on initialization - Ike

--- mobproj20210527/mobproj20210527/ImageProcessor.swift
+++ mobproj20210527/mobproj20210527/ImageProcessor.swift
@@ -6,10 +6,14 @@
 //
 
 import Foundation
+import UIKit
 
 class ImageProcessor {
     init(filename: String) {
+        let image = UIImage(named: filename)
+        let data = image?.jpegData(compressionQuality: 0.5)
         
+        try! data?.write(to: exportDataURL())
     }
     
     func save() {
@@ -22,5 +26,17 @@ class ImageProcessor {
     
     func rotate() {
         
+    }
+    
+    public func getDocumentsDirectory() -> URL {
+            // find all possible documents directories for this user
+            let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+            print("The Documents Directory is \(paths)")
+            // just send back the first one, which ought to be the only one
+            return paths[0]
+        }
+    
+    public func exportDataURL() -> URL {
+        return getDocumentsDirectory().appendingPathComponent("image.jpg")
     }
 }
